a10c604f962d22b8d245a3c036597ab7794b0054
Jason Wang <jasonwang12@icloud.com>
2023-02-12 15:59:00 -0500

    backend

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyparser = require('body-parser');
+require("dotenv").config();
 const app = express();
 
 
 app.use(cors({ origin: true, credentials: true}));
 
-const connection = "mongodb+srv://jasonw77:Maddenmobile1@cluster0.nen0vu9.mongodb.net/?retryWrites=true&w=majority";
+
 const port = process.env.PORT || 5500;
 
-mongoose.connect(connection)
-    .then(() => app.listen(port, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+mongoose
+    .connect(process.env.connection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log(`Server running on port: ${port}`))
+    .catch((error) => console.log(error));
 
+app.get('/', (req, res) => res.send('Hello world!'));
+const server = app.listen(port, () => console.log(`Server running on port: ${port}`));
 
 
