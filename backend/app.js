5ee27c3b4fbd8304229afcdfd4ee4c57fda2ee44
Jason Wang <jasonwang12@icloud.com>
2023-02-12 15:30:05 -0500

    backe

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const bodyparser = require('body-parser');
 const app = express();
+
+
+app.use(cors({ origin: true, credentials: true}));
+
+const connection = "mongodb+srv://jasonw77:Maddenmobile1@cluster0.nen0vu9.mongodb.net/?retryWrites=true&w=majority";
+const port = process.env.PORT || 5500;
+
+mongoose.connect(connection)
+    .then(() => app.listen(port, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
+
