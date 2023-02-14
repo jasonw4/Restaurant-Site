9b65ce8a140a6b5aa8d80963d7d1c50636f0c1a0
Jason Wang <jasonwang12@icloud.com>
2023-02-14 14:21:00 -0500

    trying

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component, NavWrapper } from 'react';
+import { Link } from 'react-router-dom'
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                </ul>
+        )
+
+
+    }
+
+
+
+
+
+
+
+}
